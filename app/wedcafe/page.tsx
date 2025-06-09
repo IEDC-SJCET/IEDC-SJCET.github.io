a3a0a506f292f179b38998ebe0985482240c46f7
Milansuman <milansuman2016@gmail.com>
2025-06-09 16:21:48 +0530

    add wednesday cafe signup page

---
+++ app/wedcafe/page.tsx
@@ -0,0 +1,13 @@
+
+export default function WedCafe(){
+  return (
+    <div className="w-screen h-screen">
+       <iframe
+          title="Wednesday Cafe"
+          src="https://tally.so/r/n0gJe0"
+          frameBorder="0"
+          className="absolute top-0 left-0 h-full w-full border-0 bg-black"
+        />
+    </div>
+  )
+}
