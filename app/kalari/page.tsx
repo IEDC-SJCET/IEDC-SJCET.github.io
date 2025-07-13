622a043ffe58526732ffaf924c914ff20fdedf70
Milansuman <milansuman2016@gmail.com>
2025-07-13 17:14:17 +0530

    add kalari form

---
+++ app/kalari/page.tsx
@@ -0,0 +1,12 @@
+export default function Kalari(){
+  return (
+    <div className="w-screen h-screen">
+       <iframe
+          title="Wednesday Cafe"
+          src="https://tally.so/r/3xbqpd"
+          frameBorder="0"
+          className="absolute top-0 left-0 h-full w-full border-0 bg-black"
+        />
+    </div>
+  )
+}
