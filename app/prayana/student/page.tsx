9d98edf16c14726d6651f7feb4b1eeaf2882ca39
Abhijith S <abhijiths2028@cs.sjcetpalai.ac.in>
2025-08-17 11:00:13 +0530

    Prayana student registration form

---
+++ app/prayana/student/page.tsx
@@ -0,0 +1,12 @@
+export default function WedCafe(){
+  return (
+    <div className="w-screen h-screen">
+       <iframe
+          title="Prayana Student Registration"
+          src="https://tally.so/r/wAgdoB"
+          frameBorder="0"
+          className="absolute top-0 left-0 h-full w-full border-0 bg-black"
+        />
+    </div>
+  )
+}
