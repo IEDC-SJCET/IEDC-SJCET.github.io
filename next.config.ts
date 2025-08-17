c9d19fcf4d4b16497a2834dbd6d31f59e4cc4055
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-08-17 14:02:18 +0530

    fix: trailing slash

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
