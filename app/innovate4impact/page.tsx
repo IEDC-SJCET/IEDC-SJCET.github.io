2d79383f402ad1b4820bbea5125e6c74b5d66cb6
Milansuman <milansuman2016@gmail.com>
2026-01-03 12:25:24 +0530

    add innovate4impact

---
+++ app/innovate4impact/page.tsx
@@ -0,0 +1,12 @@
+export default function Kalari() {
+  return (
+    <div className="w-screen h-screen">
+      <iframe
+        title="Innovate for Impact"
+        src="https://tally.so/r/44J7Kk"
+        frameBorder="0"
+        className="absolute top-0 left-0 h-full w-full border-0 bg-black"
+      />
+    </div>
+  )
+}
