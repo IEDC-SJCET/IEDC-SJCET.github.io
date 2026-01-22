cd2ba8f54b1b797363aad1feac1c782d9b2d49aa
Abhijith S <abhijiths2028@cs.sjcetpalai.ac.in>
2026-01-22 21:43:17 +0530

    Update iframe title for Execom Call 2026

--- app/execom/page.tsx
+++ app/execom/page.tsx
@@ -2,7 +2,7 @@ export default function Kalari(){
   return (
     <div className="w-screen h-screen">
        <iframe
-          title="Wednesday Cafe"
+          title="IEDC Execom Call 2026"
           src="https://tally.so/r/MeEzRY"
           frameBorder="0"
           className="absolute top-0 left-0 h-full w-full border-0 bg-black"
